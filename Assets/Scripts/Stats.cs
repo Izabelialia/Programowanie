671d93505649e0dcdc7302a8b87582601bc82d0a
Rajo25 <krysztalowysmok24@gmail.com>
2025-07-03 14:52:44 +0200

    Najbardziej Basic Stats
    dodałem bazowe staty które będą jeszcze dużo wymagać do dodania

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    [SerializeField]
+    private int baseValue;
+    
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+        return finalValue;
+    }
+}
