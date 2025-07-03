030ff979beef02fb46c420ab9654d0b68424fb15
Rajo25 <krysztalowysmok24@gmail.com>
2025-07-03 02:57:24 +0200

    Working Movement
    Movement dziala perfekcyjnie więc robię buckup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,27 @@
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float movementSpeed;
+    public float stopSpeed;
+    
+    public float groundDrag;
+    
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public float fallMultiplier;
+    bool canJump;
+    
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
     
     public Transform orientation;
     
@@ -17,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        canJump = true;
     }
 
     private void FixedUpdate()
@@ -26,17 +45,69 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight + 0.2f, whatIsGround);
         MyInput();
+        SpeedControl();
+        BetterJump();
+        if(grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
     }
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && canJump && grounded)
+        {
+            canJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * (movementSpeed * Time.deltaTime * 100), ForceMode.Force);
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * (movementSpeed * Time.deltaTime * 1000f), ForceMode.Force);
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * (movementSpeed * Time.deltaTime * 1000f * airMultiplier), ForceMode.Force);
+        
+        if (horizontalInput == 0 && verticalInput == 0)
+        {
+            Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            rb.linearVelocity = Vector3.Lerp(flatVelocity, Vector3.zero, Time.deltaTime * stopSpeed) + Vector3.up * rb.linearVelocity.y;
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        if (flatVelocity.magnitude > movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * movementSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        canJump = true;
+    }
+    private void BetterJump()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.AddForce(Vector3.down * fallMultiplier, ForceMode.Acceleration);
+        }
     }
 }
